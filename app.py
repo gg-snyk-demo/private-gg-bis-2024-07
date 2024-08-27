5ecdce1ae22017a35f14b17a00ced71031bece55
Bruce Banner <bruce-banner-snyk@protonmail.com>
2024-08-27 13:03:36 +0200

    Updating app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 import antigravity
 
-SEED=cab2fb2a-958e-4d1c-be3e-99fd66ef48e3
+CERT="""-----BEGIN DSA PRIVATE KEY-----
+Proc-Type: 4,ENCRYPTED
+DEK-Info: AES-256-CBC,9C76FDB486104A24905C54FD9F7A454F
+
+gx2vEp4Rf3qLtdNo/nHByZvwSaWnenxr27rnmXdA4jEeBmg4UxR7uuENcJRLKrWT
+q2suGraldzjnKQH43XRqIH+qV1kmGNkOPariVirhNiePonmoG3uJLGc6vBgSJjnH
+c5Szr9aVAEMCyIft9YWOrVs8bF1e/p+j7ZZ+pdn5HzlZRI5/a8s+y+IMxtdS0clb
+aV8tsS4LISyrqc2Ye952R66qiS/C36ivfilZBOACwYpEb1FECxV3AlglLOHm5fy1
+3xRj++gqchMAwNyAU5XCtkq89jHvGLuEGRTZEw+0t1S4qUZam8D/OchafV7OxTbq
+FRN1WX93cx3iE5JutT2/Zu0Q8QRyx4S89cAApatr2Xd5jfoB8hkTXLhtVIBsp4yr
+izK8n5DI00MqErFb+DQX6y3gt84iBQbXQMyXdSqsx3hB2JmfgysCDDEZUlm1uO0C
+X/13X9N/9O/8fYulcj1PYC8yZHG01RorLvtcA2RslUOH3ZZ59LiF0xC9WAvp6b/7
+M5g+85cs5djCfEul/w0CjMHK4edze/zhFXOVwlk2eD8LAS5SltpkitsNRFmFONsx
+Y+o2MuSiqhnGvFCTskZl5g==
+-----END DSA PRIVATE KEY-----
+"""
 
 def main():
     antigravity.fly()
